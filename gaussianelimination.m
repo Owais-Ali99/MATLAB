639a2aa23e1839c0f1bf2702f747d634e32ec896
Owais Ali <94829962+Owais-Ali99@users.noreply.github.com>
2022-05-04 22:06:49 +0500

    Update gaussianelimination.m

--- gaussianelimination.m
+++ gaussianelimination.m
@@ -1,17 +1,20 @@
+function A = gaussianelimination(A)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%   OWAIS ALI. University of Sindh, Pakistan   %%%%%%
 %%%%%%   Email: owaisshoukat99@gmail.com            %%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-A = input('Enter the matrix: ');
 [a, b] = size(A);
 n = min([a, b]);
 
 for i = 1:n
-A(i,:) = A(i,:)/A(i,i);
+    if A(i,i) ~= 0
+    A(i,:) = A(i,:)/A(i,i);
 for j = (i+1):a
-A(j,:) = A(j,:)-A(j,i)*A(i,:);
+    A(j,:) = A(j,:)-A(j,i)*A(i,:);
 end
+    end
 end
 
-display(A)
+end
